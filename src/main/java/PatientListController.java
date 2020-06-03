8db63b5922e085af5a90a9732baf9e146774174f
dddddddddtdd <kozjach@gmail.com>
2020-06-03 16:02:48 +0200

    test $everything

--- src/main/java/PatientListController.java
+++ src/main/java/PatientListController.java
@@ -15,8 +15,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -51,6 +50,7 @@ public class PatientListController implements Initializable {
                 .execute();
         fhirpatients.addAll(BundleUtil.toListOfResources(ctx, bundle));
 
+        System.out.println("test");
 // Load the subsequent pages
         while (bundle.getLink(IBaseBundle.LINK_NEXT) != null) {
             bundle = client
@@ -63,10 +63,11 @@ public class PatientListController implements Initializable {
         System.out.println("Loaded " + fhirpatients.size() + " patients!");
 
         ArrayList<PatientModel> tempPatients = new ArrayList<PatientModel>();
-        for(int i=0; i<fhirpatients.size(); i++){
+        for (int i = 0; i < fhirpatients.size(); i++) {
             Patient r4patient = (Patient) fhirpatients.get(i);
             System.out.println(r4patient.getName().get(0).getGivenAsSingleString());
-            tempPatients.add(new PatientModel(r4patient.getName().get(0).getGivenAsSingleString()));
+            tempPatients.add(new PatientModel(r4patient.getName().get(0).getGivenAsSingleString() + " " + r4patient.getName().get(0).getFamily()));
+
         }
         patients = FXCollections.observableArrayList(tempPatients);
         TableColumn<PatientModel, String> namecol = (TableColumn<PatientModel, String>) mainTable.getColumns().get(1);
@@ -74,6 +75,54 @@ public class PatientListController implements Initializable {
         namecol.setCellValueFactory(new PropertyValueFactory<PatientModel, String>("name"));
         System.out.println(patients.size());
         mainTable.setItems(patients);
+
+
+        //testy
+        System.out.println("ehheh");
+        try {
+            Patient r4patient = (Patient) fhirpatients.get(0);
+//            System.out.println("test: " + r4patient.getId());
+            Parameters outParams = client
+                    .operation()
+                    .onInstance(new IdType("Patient", r4patient.getIdElement().getIdPart()))
+                    .named("$everything")
+                    .withNoParameters(Parameters.class).useHttpGet()
+                    .execute();
+
+
+            Bundle result = (Bundle) outParams.getParameterFirstRep().getResource();
+
+            while (result.getLink("next") != null) {
+                result.getEntry().forEach(e -> {
+                    Resource res = e.getResource();
+                    switch (res.getClass().getSimpleName()) {
+                        case "MedicationRequest": {
+                            MedicationRequest medReq = (MedicationRequest) res;
+                            System.out.println("MedicationRequest: " + medReq.getRequester().getDisplay());
+                            break;
+                        }
+                        case "Observation": {
+                            Observation obs = (Observation) res;
+                            System.out.println("Observation: "+obs.getCode().getText()+ " - "+obs.getValueQuantity().getValue());
+                            break;
+                        }
+                        case "Medication": {
+                            Medication med = (Medication) res;
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                });
+                result = client.loadPage().next(result).execute();
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void add(ActionEvent actionEvent) {
@@ -81,7 +130,7 @@ public class PatientListController implements Initializable {
 
     public void showDetails(ActionEvent actionEvent) {
         //TODO getowanie zaznaczonego wiersza - pacjenta
-        Platform.runLater( () -> {
+        Platform.runLater(() -> {
             try {
                 Main.getMainStage().setScene(new Scene(FXMLLoader.load(getClass().getResource("./patient_details.fxml"))));
             } catch (IOException e) {
@@ -96,14 +145,13 @@ public class PatientListController implements Initializable {
 
     public void filtering(KeyEvent onKeyReleased) {
         filtered_patients.clear();
-        for(PatientModel patient : patients) {
+        for (PatientModel patient : patients) {
             if (patient.name.toLowerCase().contains(filter_text_box.getText().toLowerCase())) {
                 filtered_patients.add(patient);
             }
         }
         mainTable.setItems(FXCollections.observableArrayList(filtered_patients));
     }
-
 
 
     public void exit(ActionEvent actionEvent) {
