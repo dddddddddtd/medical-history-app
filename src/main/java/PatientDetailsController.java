9418e3cbbd5565134b4e455a27db85f0cb29208b
dddddddddtdd <kozjach@gmail.com>
2020-06-05 20:43:52 +0200

    testing resources related to patient

--- src/main/java/PatientDetailsController.java
+++ src/main/java/PatientDetailsController.java
@@ -1,34 +1,87 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.r4.model.*;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class PatientDetailsController implements Initializable {
     private PatientModel patient;
-    private List<Observation> observations;
-    private List<Medication> medications;
-    private List<MedicationRequest> medicationRequests;
+    private List<Observation> observations = new ArrayList<>();
+    private List<Medication> medications = new ArrayList<>();
+    private List<MedicationRequest> medicationRequests = new ArrayList<>();
 
     @FXML
-    Text texttest;
+    Text patientText;
+
+    @FXML
+    Button medReqButton;
+
+    @FXML
+    Button medButton;
+
+    @FXML
+    Button obsButton;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    public void setup(){
-        texttest.setText(patient.getName());
+    public void setup() {
+        patientText.setText(patient.getName());
         Patient r4patient = patient.getPatient();
         List<Resource> resources = FhirHandler.getPatientEverything(r4patient);
-        medicationRequests=  resources.stream().filter(x-> x.getClass().getSimpleName() == "MedicationRequest").map(x -> (MedicationRequest) x).collect(Collectors.toList());
-        medications=resources.stream().filter(x-> x.getClass().getSimpleName() == "Medication").map(x -> (Medication) x).collect(Collectors.toList());
-        observations=resources.stream().filter(x-> x.getClass().getSimpleName() == "Observation").map(x -> (Observation) x).collect(Collectors.toList());
+
+        for (Resource res : resources){
+            switch (res.getClass().getSimpleName()){
+                case "MedicationRequest":
+                    medicationRequests.add((MedicationRequest) res);
+                    break;
+                case "Medication":
+                    medications.add((Medication) res);
+                    break;
+                case "Observation":
+                    observations.add((Observation) res);
+                    break;
+            }
+        }
+        System.out.println(medicationRequests.size());
+        System.out.println(medications.size());
+        System.out.println(observations.size());
+    }
+
+    public void printMedReq() {
+        System.out.println(medicationRequests.size());
+        medicationRequests.forEach(medReq -> System.out.println("MedicationRequest: " + medReq.getRequester().getDisplay()));
+    }
+
+    public void printMed() {
+        System.out.println(medications.size());
+//        medications.forEach(med -> System.out.println(med.));
+    }
+
+    public void printObs() {
+        System.out.println(observations.size());
+        Integer i = 0;
+        for ( Observation obs : observations){
+            try {
+                System.out.println(i+". Observation: " + obs.getCode().getText() + " - " + obs.getValueQuantity().getValue());
+            } catch (FHIRException e) {
+                try {
+                    System.out.println(i+". Observation: " + obs.getCode().getText() + " - " + obs.getValueCodeableConcept().getText());
+                } catch (FHIRException fhirException) {
+                    fhirException.printStackTrace();
+                }
+            }
+            i++;
+        }
     }
 
     public void setPatient(PatientModel patient) {
